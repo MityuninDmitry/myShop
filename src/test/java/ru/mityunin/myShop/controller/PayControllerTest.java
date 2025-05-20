bece8a5b3608ff77e62ed1a7ee4e6abfe05f7738
dmitrymityunin <mity-ml@mail.ru>
2025-05-21 01:35:46 +0500

    Восстановил тесты PayControllerTest

--- src/test/java/ru/mityunin/myShop/controller/PayControllerTest.java
+++ src/test/java/ru/mityunin/myShop/controller/PayControllerTest.java
@@ -5,14 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
 import ru.mityunin.myShop.SpringBootPostgreSQLBase;
 import ru.mityunin.myShop.service.PayService;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,11 +34,19 @@ public class PayControllerTest extends SpringBootPostgreSQLBase {
 
     @Test
     public void payOrderWithOrderId() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.post("/pay")
-//                .param("order_id","1"))
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/order/1"));
-//
-//        verify(payService).setPaidFor(Long.valueOf(1));
+        when(payService.setPaidFor(anyLong())).thenReturn(Mono.empty());
+
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("order_id", "1");
+
+        webTestClient.post().uri("/pay")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .body(BodyInserters.fromFormData(formData))
+                .exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueEquals("Location", "/order/basket");
+
+        verify(payService).setPaidFor(1L);
+
     }
 }
